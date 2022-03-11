784f989baae1a7a0e87693b6e8c6741821f0a744
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2022-03-11 22:05:46 +0100

    fix bug

--- app.js
+++ app.js
@@ -6,37 +6,45 @@ const dotenv = require("dotenv");
 const axios = require("axios");
 var request = require("request");
 const https = require("https");
+const cors = require("cors");
 
 dotenv.config({ path: path.resolve(__dirname, "./dotenv/.env") });
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 app.get("/api/*", (req, res) => {
-    let requestedPath = req.path.substring("/api".length);
+    try {
+        let requestedPath = req.path.substring("/api".length);
 
-    let url = `https://api.nomics.com/v1${requestedPath}`;
+        let url = `https://api.nomics.com/v1${requestedPath}`;
 
-    url += `?key=${process.env.NOMICS_API_KEY}`;
+        url += `?key=${process.env.NOMICS_API_KEY}`;
 
-    Object.keys(req.query).forEach(
-        (key) => (url += `&${key}=${req.query[key]}`)
-    );
+        Object.keys(req.query).forEach(
+            (key) => (url += `&${key}=${req.query[key]}`)
+        );
 
-    https.get(url, (response) => {
-        response
-            .on("data", (chunk) => {
-                res.write(chunk);
-            })
-            .on("error", (error) => {
-                console.log(error);
-                res.send("error");
-            })
-            .on("end", () => {
-                res.end();
-            });
-    });
+        https.get(url, (response) => {
+            response
+                .on("data", (chunk) => {
+                    res.write(chunk);
+                })
+                .on("error", (error) => {
+                    console.log(error);
+                    res.send("error");
+                })
+                .on("end", () => {
+                    res.end();
+                });
+        });
+    } catch (err) {
+        console.log(err);
+        res.end();
+    }
 });
 
 app.listen(process.env.PORT, () =>
