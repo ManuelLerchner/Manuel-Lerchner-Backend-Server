1d33ee9255a2209be140c28ca1d5f210521149d7
Manuel <manuel.lerchner1111@gmail.com>
2022-03-13 23:20:19 +0100

    cleanup

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express();
 
 const path = require("path");
 const dotenv = require("dotenv");
-const axios = require("axios");
-var request = require("request");
+
 const https = require("https");
 const cors = require("cors");
 
@@ -13,40 +12,40 @@ dotenv.config({ path: path.resolve(__dirname, "./dotenv/.env") });
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Mock-Trading-Backend");
 });
 
 app.get("/api/*", (req, res) => {
-    try {
-        let requestedPath = req.path.substring("/api".length);
+  try {
+    let requestedPath = req.path.substring("/api".length);
 
-        let url = `https://api.nomics.com/v1${requestedPath}`;
+    let url = `https://api.nomics.com/v1${requestedPath}`;
 
-        url += `?key=${process.env.NOMICS_API_KEY}`;
+    url += `?key=${process.env.NOMICS_API_KEY}`;
 
-        Object.keys(req.query).forEach(
-            (key) => (url += `&${key}=${req.query[key]}`)
-        );
+    Object.keys(req.query).forEach(
+      (key) => (url += `&${key}=${req.query[key]}`)
+    );
 
-        https.get(url, (response) => {
-            response
-                .on("data", (chunk) => {
-                    res.write(chunk);
-                })
-                .on("error", (error) => {
-                    console.log(error);
-                    res.send("error");
-                })
-                .on("end", () => {
-                    res.end();
-                });
+    https.get(url, (response) => {
+      response
+        .on("data", (chunk) => {
+          res.write(chunk);
+        })
+        .on("error", (error) => {
+          console.log(error);
+          res.send("error");
+        })
+        .on("end", () => {
+          res.end();
         });
-    } catch (err) {
-        console.log(err);
-        res.end();
-    }
+    });
+  } catch (err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.listen(process.env.PORT, () =>
-    console.log(`Example app listening on port ${process.env.PORT}!`)
+  console.log(`Example app listening on port ${process.env.PORT}!`)
 );
