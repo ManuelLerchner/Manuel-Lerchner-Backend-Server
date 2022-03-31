256d6a70f1b52d0f0fffadbb9bc63bc9ebc945f9
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2022-03-31 19:17:17 +0200

    fix

--- routes/expenseTracker/helper.js
+++ routes/expenseTracker/helper.js
@@ -61,7 +61,7 @@ const performDatabaseAction = async function (res, func) {
         await func(conn);
     } catch (err) {
         console.log(err);
-        res.status(500).send("Error: " + err.coden);
+        res.status(500).send("MariaDB Error: " + err.coden);
     } finally {
         conn.release();
     }
